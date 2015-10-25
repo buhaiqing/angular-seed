4ad052fde3de9acbb3d398f59d63ca263235e991
hai-qing.bu <haiqing.bu@gmail.com>
2015-10-25 10:37:26 +0800

    add karma-coverage support for demo

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     frameworks: ['jasmine'],
 
-    // browsers: ['PhantomJS'],
-    browsers: ['PhantomJS', 'Chrome', 'Firefox'],
+    browsers: ['PhantomJS'],
+    //browsers: ['PhantomJS', 'Chrome', 'Firefox'],
 
     // you can define custom flags 
     customLaunchers: {
@@ -43,12 +43,56 @@ module.exports = function(config) {
       'karma-firefox-launcher',
       'karma-phantomjs-launcher',
       'karma-jasmine',
+	  'karma-coverage',
       'karma-junit-reporter'
     ],
-    reporters: ['progress', 'junit'],
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/**/*.js': ['coverage']
+    },
+    reporters: ['progress', 'junit', 'coverage'],
     junitReporter: {
-      outputFile: 'test_out/unit.xml',
+      outputFile: 'build/unit/unit.xml',
       suite: 'unit'
+    },
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'build/reports/coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        {
+          type: 'html',
+          subdir: 'report-html'
+        }, {
+          type: 'lcov',
+          subdir: 'report-lcov'
+        },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        {
+          type: 'cobertura',
+          subdir: '.',
+          file: 'cobertura.txt'
+        }, {
+          type: 'lcovonly',
+          subdir: '.',
+          file: 'report-lcovonly.txt'
+        }, {
+          type: 'teamcity',
+          subdir: '.',
+          file: 'teamcity.txt'
+        }, {
+          type: 'text',
+          subdir: '.',
+          file: 'text.txt'
+        }, {
+          type: 'text-summary',
+          subdir: '.',
+          file: 'text-summary.txt'
+        },
+      ]
     }
 
   });
