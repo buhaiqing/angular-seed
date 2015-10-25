b4122ed95a887dbe8eb22414512d8e42074b96c7
hai-qing.bu <haiqing.bu@gmail.com>
2015-10-25 09:00:16 +0800

    update browser info to support PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,9 @@
-module.exports = function(config){
+module.exports = function(config) {
   config.set({
 
-    basePath : './',
+    basePath: './',
 
-    files : [
+    files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
@@ -11,20 +11,42 @@ module.exports = function(config){
       'app/view*/**/*.js'
     ],
 
-    autoWatch : true,
+    autoWatch: true,
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers: ['PhantomJS'],
+	//browsers: ['PhantomJS', 'Chrome'],
+ 
+    // you can define custom flags 
+    customLaunchers: {
+      'PhantomJS_custom': {
+        base: 'PhantomJS',
+        options: {
+          windowName: 'my-window',
+          settings: {
+            webSecurityEnabled: false
+          },
+        },
+        flags: ['--load-images=true'],
+        debug: true
+      }
+    },
+ 
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom) 
+      exitOnResourceError: true
+    },
 
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
-
-    junitReporter : {
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+	  'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
+    reporters:['progress', 'junit'],
+    junitReporter: {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
     }
