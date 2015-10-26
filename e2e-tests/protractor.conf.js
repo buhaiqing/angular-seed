d27a90cf763f37d8d5849ad1883f5dc6139bce1c
hai-qing.bu <haiqing.bu@gmail.com>
2015-10-26 16:14:12 +0800

    add support on Protractor HTML report

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,3 +1,5 @@
+var HtmlReporter = require('protractor-html-screenshot-reporter');
+
 exports.config = {
   allScriptsTimeout: 11000,
 
@@ -15,5 +17,15 @@ exports.config = {
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+
+
+  //  for html screenshot reporter
+  // link: https://www.npmjs.com/package/protractor-html-screenshot-reporter
+  onPrepare: function() {
+    // Add a screenshot reporter and store screenshots to `./screnshots`: 
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: './screenshots'
+    }));
   }
 };
